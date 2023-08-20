59e2d3adf55b305063ef7fd3e95ce7e0497da3e4
Charlie <gamerboy030607@gmail.com>
2023-08-21 08:49:22 +1000

    upload first virtual rt file

---
+++ Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+namespace virtualRT
+{
+    public class NoteData
+    {
+        public int NotePitch { get; set; }
+        public string NoteColor { get; set; }
+        public int Velocity { get; set; }
+        public double XCoord { get; set; }
+        public double YCoord { get; set; }
+        public double ZCoord { get; set; }
+        public double Opacity { get; set; }
+        public double Brightness { get; set; }
+
+        public NoteData(int notePitch, string noteColor, int velocity, double xCoord, double yCoord, double zCoord, double opacity, double brightness)
+        {
+            NotePitch = notePitch;
+            NoteColor = noteColor;
+            Velocity = velocity;
+            XCoord = xCoord;
+            YCoord = yCoord;
+            ZCoord = zCoord;
+            Opacity = opacity;
+            Brightness = brightness;
+        }
+        public class NoteDataSerializer
+    {
+        public void Serialize(List<NoteData> noteDataList, string filePath)
+        {
+            string jsonString = JsonSerializer.Serialize(noteDataList);
+            File.WriteAllText(filePath, jsonString);
+        }
+
+        public List<NoteData> Deserialize(string filePath)
+        {
+            string jsonString = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<List<NoteData>>(jsonString);
+        }
+    }
+    public class NoteDataList
+    {
+        private List<NoteData> _noteDataList;
+
+        public NoteDataList()
+        {
+            _noteDataList = new List<NoteData>();
+        }
+
+        public void AddNoteData(NoteData noteData)
+        {
+            _noteDataList.Add(noteData);
+        }
+
+        public void AddNoteDataList(List<NoteData> noteDataList)
+        {
+            _noteDataList.AddRange(noteDataList);
+        }
+
+        public List<NoteData> GetNoteDataList()
+        {
+            return _noteDataList;
+        }
+    }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+using System.Text.Json;
+using virtualRT;
+using static virtualRT.NoteData;
+
+namespace virtualwriter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NoteDataList noteDataList = new NoteDataList();
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            noteDataList.AddNoteData(new NoteData(60, "red", 100, 1.0, 2.0, 3.0, 0.5, 0.8));
+            noteDataList.AddNoteData(new NoteData(61, "blue", 90, 2.0, 3.0, 4.0, 0.6, 0.9));
+            noteDataList.AddNoteData(new NoteData(62, "green", 80, 3.0, 4.0, 5.0, 0.7, 0.6));
+            NoteDataSerializer serializer = new NoteDataSerializer();
+            serializer.Serialize(noteDataList.GetNoteDataList(), "noteData.json");
+            printthing();
+        }
+        static void printthing()
+        {
+            NoteDataSerializer serializer = new NoteDataSerializer();
+            List<NoteData> noteDataList = serializer.Deserialize("noteData.json");
+            foreach (NoteData noteData in noteDataList)
+            {
+                Console.WriteLine($"NotePitch: {noteData.NotePitch}");
+                Console.WriteLine($"NoteColor: {noteData.NoteColor}");
+                Console.WriteLine($"Velocity: {noteData.Velocity}");
+                Console.WriteLine($"XCoord: {noteData.XCoord}");
+                Console.WriteLine($"YCoord: {noteData.YCoord}");
+                Console.WriteLine($"ZCoord: {noteData.ZCoord}");
+                Console.WriteLine($"Opacity: {noteData.Opacity}");
+                Console.WriteLine($"Brightness: {noteData.Brightness}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
